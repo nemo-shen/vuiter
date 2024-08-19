82621b600e2d1a5128fcecaecb4b778312800f33
Nemo <nemoshen94@gmail.com>
2024-08-19 13:19:53 +0800

    WIP

--- templates/my-vuiter-app/vite.config.ts
+++ templates/my-vuiter-app/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
 
 export default defineConfig({
   build: {
@@ -19,8 +18,5 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-    nodeResolve({
-      exportConditions: ["node"],
-    }),
   ],
 });
